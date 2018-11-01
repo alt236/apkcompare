3dd425fc94b595eda2a29e2485107c1b6008a09b
Alexandros Schillings <aschillings@gmail.com>
2018-11-01 18:52:11 +0000

    Certificate comparisons are now delivered via a CompositeResult

--- src/main/java/uk/co/alt236/apkcompare/comparators/signature/SignatureBlockFactory.java
+++ src/main/java/uk/co/alt236/apkcompare/comparators/signature/SignatureBlockFactory.java
@@ -3,6 +3,7 @@ package uk.co.alt236.apkcompare.comparators.signature;
 import uk.co.alt236.apkcompare.comparators.results.ComparisonResult;
 import uk.co.alt236.apkcompare.comparators.results.ResultBlock;
 import uk.co.alt236.apkcompare.comparators.results.comparisons.Comparison;
+import uk.co.alt236.apkcompare.comparators.results.comparisons.CompositeResult;
 import uk.co.alt236.apkcompare.comparators.results.comparisons.DateComparison;
 import uk.co.alt236.apkcompare.comparators.results.comparisons.TypedComparison;
 import uk.co.alt236.apkcompare.repo.signature.SigningCertificate;
@@ -74,65 +75,65 @@ class SignatureBlockFactory {
                 cert2 = null;
             }
 
-            final List<Comparison> comparisons = compareCertificates(cert1, cert2);
-            blockList.add(new ResultBlock("Certificate " + (i + 1), comparisons));
+            final CompositeResult compositeResult = compareCertificates("Certificate (" + (i + 1) + "/" + maxSize + ")", cert1, cert2);
+            blockList.add(compositeResult);
         }
 
         return blockList;
     }
 
-    private List<Comparison> compareCertificates(SigningCertificate cert1,
-                                                 SigningCertificate cert2) {
+    private CompositeResult compareCertificates(final String title,
+                                                SigningCertificate cert1,
+                                                SigningCertificate cert2) {
 
-        final List<Comparison> retVal = new ArrayList<>();
-
-        retVal.add(new TypedComparison<>(
-                "Subject",
-                null,
-                cert1 == null ? null : cert1.getSubjectDN(),
-                cert2 == null ? null : cert2.getSubjectDN()));
-        retVal.add(new TypedComparison<>(
-                "Issuer",
-                null,
-                cert1 == null ? null : cert1.getIssuerDN(),
-                cert2 == null ? null : cert2.getIssuerDN()));
-        retVal.add(new TypedComparison<>(
-                "Serial",
-                null,
-                cert1 == null ? null : cert1.getSerialNumber(),
-                cert2 == null ? null : cert2.getSerialNumber()));
-        retVal.add(new TypedComparison<>(
-                "Algorithm",
-                null,
-                cert1 == null ? null : cert1.getSigAlgName(),
-                cert2 == null ? null : cert2.getSigAlgName()));
-        retVal.add(new DateComparison(
-                "Valid from",
-                null,
-                cert1 == null ? null : cert1.getNotBefore(),
-                cert2 == null ? null : cert2.getNotBefore()));
-        retVal.add(new DateComparison(
-                "Valid to",
-                null,
-                cert1 == null ? null : cert1.getNotAfter(),
-                cert2 == null ? null : cert2.getNotAfter()));
-        retVal.add(new TypedComparison<>(
-                "MD5 Thumb",
-                null,
-                cert1 == null ? null : cert1.getMd5Thumbprint(),
-                cert2 == null ? null : cert2.getMd5Thumbprint()));
-        retVal.add(new TypedComparison<>(
-                "SHA1 Thumb",
-                null,
-                cert1 == null ? null : cert1.getSha1Thumbprint(),
-                cert2 == null ? null : cert2.getSha1Thumbprint()));
-        retVal.add(new TypedComparison<>(
-                "SHA256 Thumb",
-                null,
-                cert1 == null ? null : cert1.getSha256Thumbprint(),
-                cert2 == null ? null : cert2.getSha256Thumbprint()));
-
-        return retVal;
+        return new CompositeResult.Builder()
+                .withTitle(title)
+                .withComparison(new TypedComparison<>(
+                        title,
+                        "Subject",
+                        cert1 == null ? null : cert1.getSubjectDN(),
+                        cert2 == null ? null : cert2.getSubjectDN()))
+                .withComparison(new TypedComparison<>(
+                        title,
+                        "Issuer",
+                        cert1 == null ? null : cert1.getIssuerDN(),
+                        cert2 == null ? null : cert2.getIssuerDN()))
+                .withComparison(new TypedComparison<>(
+                        title,
+                        "Serial",
+                        cert1 == null ? null : cert1.getSerialNumber(),
+                        cert2 == null ? null : cert2.getSerialNumber()))
+                .withComparison(new TypedComparison<>(
+                        title,
+                        "Algorithm",
+                        cert1 == null ? null : cert1.getSigAlgName(),
+                        cert2 == null ? null : cert2.getSigAlgName()))
+                .withComparison(new DateComparison(
+                        title,
+                        "Valid from",
+                        cert1 == null ? null : cert1.getNotBefore(),
+                        cert2 == null ? null : cert2.getNotBefore()))
+                .withComparison(new DateComparison(
+                        title,
+                        "Valid to",
+                        cert1 == null ? null : cert1.getNotAfter(),
+                        cert2 == null ? null : cert2.getNotAfter()))
+                .withComparison(new TypedComparison<>(
+                        title,
+                        "MD5 Thumb",
+                        cert1 == null ? null : cert1.getMd5Thumbprint(),
+                        cert2 == null ? null : cert2.getMd5Thumbprint()))
+                .withComparison(new TypedComparison<>(
+                        title,
+                        "SHA1 Thumb",
+                        cert1 == null ? null : cert1.getSha1Thumbprint(),
+                        cert2 == null ? null : cert2.getSha1Thumbprint()))
+                .withComparison(new TypedComparison<>(
+                        title,
+                        "SHA256 Thumb",
+                        cert1 == null ? null : cert1.getSha256Thumbprint(),
+                        cert2 == null ? null : cert2.getSha256Thumbprint()))
+                .build();
     }
 
     interface SignatureInfoProvider {
